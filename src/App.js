ac1faf882846e09813e1ccf26a9479debe67df02
Tiago Bértolo <tiagobertolo@gmail.com>
2024-03-06 01:33:02 +0000

    Sizing.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Topbar from "./Topbar/Topbar";
 function Logo() {
   return (
     <img
-      className="h-80 w-auto"
+      className="h-50 sm:h-80 md:h-96 w-auto"
       src="https://raw.githubusercontent.com/bertolo1988/scorpion-game/main/public/new-logo.svg"
       alt="Your Company"
     />
@@ -15,8 +15,8 @@ function Logo() {
 function ComingSoon() {
   return (
     <img
-      className="h-8 w-auto"
-      src="https://raw.githubusercontent.com/bertolo1988/scorpion-game/main/src/images/coming-soon.svg"
+      className="px-5 h-auto w-64"
+      src="https://raw.githubusercontent.com/bertolo1988/scorpion-game/main/src/images/coming-soon.png"
       alt="Your Company"
     />
   );
@@ -28,14 +28,18 @@ function App() {
       <Topbar />
       <div
         style={{ marginTop: "-3rem" }}
-        class="h-screen flex items-center justify-center"
+        className="h-screen flex items-center justify-center"
       >
-        <div class="grid grid-cols-1">
-          <div className="flex items-center justify-center">
-            <Logo />
+        <div className="grid grid-cols-3">
+          <div className="col-span-3">
+            <div className="flex items-center justify-center">
+              <Logo />
+            </div>
           </div>
-          <div className="flex items-center justify-center">
-            <ComingSoon />
+          <div className="col-span-3 md:col-span-1 md:col-start-2">
+            <div className="flex items-center justify-center">
+              <ComingSoon />
+            </div>
           </div>
         </div>
       </div>
